dbf145e2869b5bd60c58ba7565fdee65939d669a
Maciej Korcz <maciejkorcz1@gmail.com>
2020-11-28 12:55:31 +0100

    xamarin local database

---
+++ mslearn-xamarin-store-data-sqlite-master/src/exercise1/start/People/PersonRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using People.Models;
+using SQLite;
+
+namespace People
+{
+    public class PersonRepository
+    {
+        private SQLiteAsyncConnection conn;
+        public string StatusMessage { get; set; }
+
+        public PersonRepository(string dbPath)
+        {
+            // TODO: Initialize a new SQLiteConnection
+            // TODO: Create the Person table
+            conn = new SQLiteAsyncConnection(dbPath);
+            conn.CreateTableAsync<Person>().Wait();
+
+        }
+
+        public async Task AddNewPersonAsync(string name)
+        {
+            int result = 0;
+            try
+            {
+                //basic validation to ensure a name was entered
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Valid name required");
+
+                // TODO: insert a new person into the Person table
+                result = await conn.InsertAsync(new Person { Name = name });
+
+                StatusMessage = string.Format("{0} record(s) added [Name: {1})", result, name);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            }
+        }
+
+        public async Task<List<Person>> GetAllPeopleAsync()
+        {
+            try
+            {
+                return await conn.Table<Person>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<Person>();
+        }
+    }
+}
